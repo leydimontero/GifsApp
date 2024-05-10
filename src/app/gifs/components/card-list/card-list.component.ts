aa181a467e5bca240d9b7056a87b58e4a6a278bb
leydi montero <leidy_940@hotmail.es>
2024-05-09 20:09:09 -0500

    inicio de proyecto GifsApp

---
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gifs-card-list',
+  templateUrl: './card-list.component.html',
+})
+
+export class CardListComponent  {
+  constructor() { }
+
+
+}
