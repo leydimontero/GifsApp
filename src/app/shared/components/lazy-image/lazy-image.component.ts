a76cecba36796107ceadf06b18f651e68ee09717
leydi montero <leidy_940@hotmail.es>
2024-05-20 17:55:03 -0500

    implementacion de componente separados ( imagen, card)

---
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'shared-lazy-image',
+  templateUrl: './lazy-image.component.html',
+
+})
+export class LazyImageComponent {
+
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt: string = '';
+
+  public hasLoaded: boolean = false
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error( 'URL property is required');
+  }
+
+  onLoad(){
+
+      this.hasLoaded = true;
+
+  }
+
+}
