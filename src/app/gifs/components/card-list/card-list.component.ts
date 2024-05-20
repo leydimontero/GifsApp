468ad8d2d9cec9def57d957c4611a15c417b101c
leydi montero <leidy_940@hotmail.es>
2024-05-20 16:14:57 -0500

    localStorange, inyeccion del servicio

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Gif } from '../../interfaces/gifs.interfaces';
 
 @Component({
   selector: 'gifs-card-list',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class CardListComponent  {
-  constructor() { }
+
+  @Input()
+
+  public gifs: Gif[] = [];
 
 
 }
--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -10,8 +10,12 @@ export class SidebarComponent {
 
   constructor ( private gifsService: GifsService ) {}
 
-  get tags() {
+  get tags(): string [] {
     return this.gifsService.tagsHistory
+  }
+
+  public searchTag(tag: string){
+    this.gifsService.searchTag( tag );
   }
 
 }
